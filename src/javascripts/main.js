300eed40052a579ea454499213a688213c7d104f
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-03-22 17:34:27 -0500

    updated color theme

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import './components/projects/projects.scss';
 import projects from './components/projects/projects';
 import navbar from './components/navbar/navbar';
 
