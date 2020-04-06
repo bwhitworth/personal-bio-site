620323a5bca251f4eb58fc8fcfb9f2607738caa1
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-06 11:47:56 -0500

    deployed firebase

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -2,22 +2,25 @@ import utils from '../../helpers/utils';
 import projectData from '../../helpers/data/projectData';
 
 const printProjectCards = () => {
-  let domString = '';
-  const projects = projectData.getProjects();
-  projects.forEach((item) => {
-    if (item.available) {
-      domString += '<div class="card project-card" style="width: 20rem">';
-      domString += `  <h5 class='card-header'>${item.title}</h5>`;
-      domString += `  <div class="img-container"><img src='${item.screenshot}' class='card-img-top' alt='project screenshot'></div>`;
-      domString += '  <div class="card-body">';
-      domString += `    <p class='card-text'>${item.description}</p>`;
-      domString += `    <p class='card-text'>Technologies Used: <br> ${item.technologiesUsed}</p>`;
-      domString += `    <a href='${item.githubUrl}' class='project-gh-btn'><i class="fab fa-github-square"></i></a>`;
-      domString += '  </div>';
-      domString += '</div>';
-    }
-  });
-  utils.printToDom('projects-container', domString);
+  projectData.getProjects()
+    .then((projects) => {
+      let domString = '';
+      projects.forEach((p) => {
+        if (p.available) {
+          domString += '<div class="card project-card" style="width: 20rem">';
+          domString += `  <h5 class='card-header text-center'>${p.title}</h5>`;
+          domString += `  <div class="img-container"><img src='${p.screenshot}' class='card-img-top' alt='project screenshot'></div>`;
+          domString += '  <div class="card-body">';
+          domString += `    <p class='card-text'>${p.description}</p>`;
+          domString += `    <p class='card-text'>Technologies Used: <br> ${p.technologiesUsed}</p>`;
+          domString += `    <a href='${p.githubUrl}' class='project-gh-btn'><i class="fab fa-github-square col-2"></i></a>`;
+          domString += `    <a href='${p.deployedUrl}' class='project-gh-btn'><i class="fas fa-desktop col-2"></i></a>`;
+          domString += '  </div>';
+          domString += '</div>';
+        }
+      });
+      utils.printToDom('projects-container', domString);
+    });
 };
 
 export default { printProjectCards };
--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -4,10 +4,13 @@ import apiKeys from '../apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getProjects = () => new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-undef
   axios.get(`${baseUrl}/projects.json`)
     .then((response) => {
+      console.error('working?');
       const theseProjects = response.data;
       const projectsArray = [];
+      console.error(theseProjects);
       Object.keys(theseProjects).forEach((projectId) => {
         theseProjects[projectId].id = projectId;
         projectsArray.push(theseProjects[projectId]);
