aaf01565bbfdfc02f2488efc9d5889114a523940
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-04-05 21:13:58 -0500

    firebase deploy

--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,52 +1,20 @@
-const projectsArray = [
-  {
-    title: 'Sorting Hat',
-    screenshot: 'https://github.com/bwhitworth/sorting-hat/raw/master/screenshots/sorting-hat-screenshot-1.png',
-    description: 'Single-page app which replicates the Sorting Hat concept from Harry Potter. Add students and sort them into random houses, then expel the bad ones to Voldermort\'s army.',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub Version Control',
-    available: true,
-    deployedUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/bwhitworth/sorting-hat',
-  },
-  {
-    title: 'Bear Tracker',
-    screenshot: 'https://github.com/bwhitworth/bear-watcher/raw/master/screenshots/screenshot-bonus-main-collapsed.png',
-    description: 'Single-page app for tracking bears and their fishing records. Individual records with timestamps of catches and failures. Fattest Bear Awards tracks bears with most fish caught.',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub Version Control',
-    available: true,
-    deployedUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/bwhitworth/bear-watcher',
-  },
-  {
-    title: 'Cool Project 1',
-    screenshot: 'https://www.gallopaint.com/wp-content/uploads/2019/11/image-placeholder.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub Version Control',
-    available: false,
-    deployedUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project 2',
-    screenshot: 'https://www.gallopaint.com/wp-content/uploads/2019/11/image-placeholder.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub Version Control',
-    available: false,
-    deployedUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-  {
-    title: 'Cool Project 3',
-    screenshot: 'https://www.gallopaint.com/wp-content/uploads/2019/11/image-placeholder.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, GitHub Version Control',
-    available: false,
-    deployedUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-  },
-];
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
 
-const getProjects = () => projectsArray;
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/projects.json`)
+    .then((response) => {
+      const theseProjects = response.data;
+      const projectsArray = [];
+      Object.keys(theseProjects).forEach((projectId) => {
+        theseProjects[projectId].id = projectId;
+        projectsArray.push(theseProjects[projectId]);
+      });
+      resolve(projectsArray);
+    })
+    .catch((err) => reject(err));
+});
 
 export default { getProjects };
